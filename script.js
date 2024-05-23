888ff0ac3cb6b79936874350dde9dac65354e442
unknown <luciamain58@gmail.com>
2024-05-23 10:29:57 -0300

    casi terminado

--- script.js
+++ script.js
@@ -12,8 +12,22 @@ function scrollFooter(scrollY, heightFooter) {
         });
     }
 }
+function bajarPaginadelTodo() {
+    const abajo = document.body.scrollHeight;
+    window.scrollTo({ top: abajo, behavior: 'smooth' });
+}
 
-$(document).ready(function() {
+function bajarPagina(idSection) {
+    const section = document.getElementById(idSection);
+    if (section) {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        const middlePosition = sectionTop + (sectionHeight / 2) - (window.innerHeight / 2);
+        window.scrollTo({ top: middlePosition, behavior: 'smooth' });
+    }
+}
+
+$(document).ready(function () {
     var windowHeight = $(window).height(),
         footerHeight = $('footer').height(),
         heightDocument = (windowHeight) + ($('.content').height()) + ($('footer').height()) - 20;
@@ -26,7 +40,7 @@ $(document).ready(function() {
     // Definiendo el tamaño de los elementos header y contenido
     $('header').css({
         'height': windowHeight + 'px',
-        'line-height': windowHeight + 'px'
+
     });
 
     $('.wrapper-parallax').css({
@@ -36,7 +50,7 @@ $(document).ready(function() {
     scrollFooter(window.scrollY, footerHeight);
 
     // Al hacer scroll
-    window.onscroll = function() {
+    window.onscroll = function () {
         var scroll = window.scrollY;
 
         $('#scroll-animate-main').css({
@@ -50,3 +64,15 @@ $(document).ready(function() {
         scrollFooter(scroll, footerHeight);
     }
 });
+function toggleMoreButtons() {
+    var moreButtons = document.querySelector('.more-buttons');
+    moreButtons.classList.toggle('show');
+  }
+
+  document.addEventListener('click', function(event) {
+    var moreButtons = document.querySelector('.more-buttons');
+    var button = event.target.closest('.button-container button');
+    if (!moreButtons.contains(event.target) && !button) {
+      moreButtons.classList.remove('show');
+    }
+  });
